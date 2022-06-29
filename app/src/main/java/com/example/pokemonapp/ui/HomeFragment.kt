28984fc1eee9877e937e3ae4d375b7077d05f07f
agathaappb <agatha.alana@hotmail.com>
2022-06-28 21:51:24 -0300

    Realizando navegação entre fragments

--- app/src/main/java/com/example/pokemonapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/pokemonapp/ui/HomeFragment.kt
@@ -6,12 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.pokemonapp.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 
 class HomeFragment : Fragment() {
@@ -21,9 +18,27 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setView()
+        eventClick()
+
+    }
+
+    private fun setView(){
+        btnPokemon = view?.findViewById(R.id.btn_list_pokemon)!!
+    }
+
+    private fun eventClick(){
+        btnPokemon.setOnClickListener {
+            findNavController().navigate(R.id.listPokemonFragment)
+        }
+    }
+
 
     companion object {
         fun newInstance() = HomeFragment()
